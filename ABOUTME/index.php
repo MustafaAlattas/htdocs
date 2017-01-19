50e86bb2951c4987eb60c4ef8a27accf47d5eb19
Mustafa Alattas <Alattas.mustafa@gmail.com>
2017-01-19 16:07:49 +0400

    Added my website

---
+++ ABOUTME/index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+<style>
+
+#header{
+    border: 2px dashed;
+    background: white;
+    padding : 20px;
+    width: 95%;
+    background-color: #CCCCCC;
+    
+}
+p {
+        padding : 20px;
+    width: 95%;
+        background-color: #CCCCCC;
+
+}
+</style>
+</head>
+<body  background ="back.jpg";>
+
+<h1 id="header"> Welcome to my page </h1>
+<img src="photo.jpg" alt="My Picture" >
+<h1 id="header"> My Name is Mustafa </h1>
+<p>
+I Was born in the 90s i love video editing And Cars </br>
+Today the Doctor said to us to create our own wep page so this is mine
+ </p>
+
+<h1 id="header"> Here is an example of my work  </h1>
+<iframe width="420" height="315"
+src="https://www.youtube.com/embed/_jKMODeWKzY">
+</iframe>
+</body>
---
+++ CIS2503/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+
+<head>
+<style>
+#header{
+    border: 2px solid;
+    background: yellow;
+    padding : 20px;
+    width: 80%;
+}
+</style>
+</head>
+<body>
+<h1 id="header"> Welcome </h1>
+<h1 id="header"> Hello </h1>
+</body>
+
